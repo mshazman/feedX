ab6b0f0592ad83404bf359d26c268dc8d8520817
Shubham Pandey <shubham.pandey@mountblue.io>
2019-10-15 11:52:15 +0530

    Add user views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,f'Account created. Now you can login!!')
+            print("form is validated")
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+        print("form is not validated!!")
+    return render(request,'user/register.html',{'form':form})
+
