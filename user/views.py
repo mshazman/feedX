c40b4989a6d9c411ba66a77d4aead4d00c4999ea
Shazman Malik <=>
2019-10-22 19:51:43 +0530

    Refactoring Code

--- user/views.py
+++ user/views.py
@@ -30,7 +30,7 @@ def pull_url(request):
 
 
 def index(request):
-    return render(request, 'user/index.html')
+    return render(request, 'index.html')
 
 
 class UserRegisterView(SuccessMessageMixin, CreateView):
@@ -101,11 +101,11 @@ def test(request, filename, id=0, hex=0):
 
 
 def handler404(request, exception):
-    return render(request, 'user/404.html', status=404)
+    return render(request, '404.html', status=404)
 
 
 '''500 custom error handling'''
 
 
 def handler500(request):
-    return render(request, 'user/500.html', status=500)
+    return render(request, '500.html', status=500)
