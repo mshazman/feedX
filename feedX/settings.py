d79b2cb4524df0c0f6cc06ee501f459c24bec2d3
Shubham Pandey <pshubham380@gmail.com>
2019-10-22 15:23:21 +0530

    load email address from environment variables

--- feedX/settings.py
+++ feedX/settings.py
@@ -159,5 +159,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'shubham.pandey@mountblue.io'
-EMAIL_HOST_PASSWORD = 'shubhuu@29'
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
